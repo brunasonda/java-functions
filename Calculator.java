efa4d5413a39e32c0ce8fe27b4da6e5d0ee7f651
Bruna Sonda <bsonda@gmail.com>
2025-05-08 15:05:17 +0200

    Add Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,10 +1,29 @@
 public class Calculator {
-    
     public static void main(String[] args) {
+        double number1 = 10;
+        double number2 = 20;
 
-        //Ejecuta aquí todos los métodos creados e imprime en la terminal. 
-        
+        System.out.println("Addition: " + add(number1, number2));
+        System.out.println("Subtraction: " + subtract(number1, number2));
+        System.out.println("Multiply: " + multiply(number1, number2));
+        System.out.println("Divide: " + divide(number1, number2));
     }
+
+    public static double add(double number1, double number2) {
+        return number1 + number2;
+    }
+
+    public static double subtract(double number1, double number2) {
+        return number1 - number2;
+    }
+
+    public static double multiply(double number1, double number2) {
+        return number1 * number2;
+    }
+    public static double divide(double number1, double number2) {
+        return number1 / number2;
+    }
+}
 
     //Define los métodos basándote en su doc comment.
 
@@ -18,7 +37,7 @@ public class Calculator {
      * Inside the function:
      * 1. sum 2 numbers
      */
-    
+
 
     /**
      * Function name: subtract
@@ -56,4 +75,3 @@ public class Calculator {
      */
     
 
-}
